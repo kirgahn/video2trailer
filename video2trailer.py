50cb0e6d3797a2ace43551a2fdc3e5da8005634f
Kirgahn <kirgahn@hotmail.com>
2015-06-24 17:30:19 +0200

    small update

--- video2trailer.py
+++ video2trailer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-# incremental version 0.1
+# incremental version 0.2
 
-#import glob
 import random
 import datetime
 import mimetypes
@@ -11,13 +10,16 @@ from moviepy.editor import *
 
 parser = argparse.ArgumentParser()
 parser.add_argument("sourcefile", help="Source video file", type=str)
+parser.add_argument("-v", "--verbose", help="Print additional info", action="store_true" )
 parser.add_argument("-s", "--sliceduration", help="Duration of each slice in seconds, if unpecified assume one second", type=int)
 parser.add_argument("-dt", "--duration", help="Total duration of output file, if unspecified assume 30 seconds", type=int)
-parser.add_argument("-v", "--verbose", help="Print additional info", action="store_true" )
 parser.add_argument("-d", "--destfile", help="Destination video file, if unspecified append \"_trailer.webm\" to source filename", type=str)
+parser.add_argument("-f", "--fps", help="Output videofile frames per second, if empty assumes source fps", type=int)
+parser.add_argument("-b", "--bitrate", help="Output videofile bitrate in \"x.x\" format, if empty assumes \"1.2M\"", type=float)
 
 args = parser.parse_args()
 sourcefile = args.sourcefile
+v = VideoFileClip(sourcefile)
 
 if not args.destfile:
 	destfile=str(args.sourcefile)+'_trailer.webm'
@@ -34,7 +36,20 @@ if not args.duration:
 else:
 	duration=args.duration
 
-#bitrate="'1.2M'"
+if not args.fps:
+        fps=int(v.fps)
+        if args.verbose:
+                print("Assuming FPS: " + str(v.fps))
+else:
+	fps=args.fps
+
+if not args.bitrate:
+        bitrate="1.2M"
+        if args.verbose:
+                print("Assuming bitrate: " + bitrate)
+else:
+        bitrate=str(args.bitrate)+"M"
+
 
 if args.verbose:
 	print("Configuration parameters")
@@ -42,8 +57,10 @@ if args.verbose:
 	print("Destination file: "+destfile)
 	print("Slice duration: "+str(sliceduration))
 	print("Webm duration: "+str(duration))
+	print("fps: "+ str(fps))
+	print("bitrate: "+ bitrate)
 
-v = VideoFileClip(sourcefile)
+
 slices = []
 prevpos = 0
 cycles = duration/sliceduration
@@ -73,4 +90,5 @@ vo = v.subclip(s,s+sliceduration)
 vo = vo.resize(width=720)
 slices.append(vo)
 
-concatenate_videoclips(slices,method='compose').write_videofile(destfile, bitrate='1.2M', fps=24)
+concatenate_videoclips(slices,method='compose').write_videofile(destfile, bitrate=bitrate, fps=fps)
+#concatenate_videoclips(slices,method='compose').write_videofile(destfile, bitrate='1M', fps=24)
