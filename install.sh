7c1803731bf58a0f6c4d3f4b62134fe577b12277
Kirgahn <kirgahn@hotmail.com>
2015-06-24 16:11:55 +0200

    added install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+# to be run as root, possibly via sudo
+cp video2gallery.py /usr/local/bin/video2gallery
+cp video2trailer.py /usr/local/bin/video2trailer
+cp video2webm.py /usr/local/bin/video2webm
+chown root. /usr/local/bin/video2*
+chmod 755 /usr/local/bin/video2*
