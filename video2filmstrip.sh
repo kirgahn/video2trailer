65e2bfd40222816fa44dbd8975d85294a5c82acc
Kirgahn <kirgahn@hotmail.com>
2015-07-16 15:27:39 +0200

    fixed a couple of thinf on video2filmstrip

--- video2filmstrip.sh
+++ video2filmstrip.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
+# requires ffmpeg and imagemagick!
 
-#ffmpeg -v 0 -ss `echo 60*$2|bc` -y -i $1 -t 1 -s 1280x720 -f image2 _tmp.jpg ;  feh _tmp.jpg ; rm _tmp.jpg
-duration_float=`ffprobe -i $i -show_format -v quiet | sed -n 's/duration=//p'`
+duration_float=`ffprobe -i $1 -show_format -v quiet | sed -n 's/duration=//p'`
 duration=`echo "scale=2;$duration_float"|bc`
 echo "video duration:" $duration
 
-timegap=`echo "scale=2;$duration/16;"|bc`
+timegap=`echo "scale=2;$duration/20;"|bc`
 echo "time gap between snapshots:" $timegap 
-
-for i in {1..16}; 
-	do ffmpeg -v 0 -ss `echo $i*$timegap|bc` -y -i $1 -t 1 -s 320x240 -f image2 _test$i.jpg; 
+framelist=""
+for i in {0..19}; 
+	do ffmpeg -v 0 -ss `echo $i*$timegap|bc` -y -i $1 -t 1 -s 320x240 -f image2 _tmp$i.jpg; 
 	echo "extracting frame number " $i
+	framelist=$framelist" _tmp$i.jpg"
+	#echo $framelist
 done; 
 
 echo "done, let's join those images!"
-montage  -tile 4 -geometry +0+0  _test* $1.jpeg
+montage  -tile 5 -geometry +0+0  $framelist $1.jpeg
 
 #feh testout.jpg
 echo "cleaning temp files.."
-rm _test*
+rm _tmp*
 
 echo "done! output file is " $1.jpeg
 
