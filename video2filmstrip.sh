66f462e1eeedac24225fcd949b720fc69d10b07e
Kirgahn <kirgahn@hotmail.com>
2015-11-12 00:54:37 +0100

    small enhancements to video2filmstrip

--- video2filmstrip.sh
+++ video2filmstrip.sh
@@ -11,23 +11,28 @@ echo "video duration:" $duration
 
 timegap=`echo "scale=2;$duration/20;"|bc`
 echo "time gap between snapshots:" $timegap 
-#hash_str="##"
+progressbar=""
 
-for i in $(seq 1 $tot_frames);
-	do ffmpeg -v 0 -ss `echo $i*$timegap|bc` -y -i "$1" -t 1 -s 320x240 -f image2 _tmp$i.jpg; 
+for i in $(seq 0 `echo $tot_frames-1|bc`);
+do
+	if [ "$i" == "0" ];
+		then
+			ffmpeg -v 0 -ss `echo $timegap/2|bc` -y -i "$1" -t 1 -s 320x240 -f image2 _tmp$i.jpg; 
+		else
+			ffmpeg -v 0 -ss `echo $i*$timegap|bc` -y -i "$1" -t 1 -s 320x240 -f image2 _tmp$i.jpg; 
+	fi
 	#echo -ne 'extracting frame number' $i' \r'
 	progress=`echo $progress+$percentage|bc`
-	#echo -ne 'extracting frames ('$progress'%)'$hash_str' \r'
-	echo -ne 'extracting frames ('$progress'%) \r'
+	progressbar=$progressbar##
+	echo -ne 'extracting frames ('$progress'%)' $progressbar' \r'
 	framelist=$framelist" _tmp$i.jpg"
 	#echo $framelist
 done; 
-#echo -ne '\n'
+echo -ne '\n'
 
 echo "done, let's join those images!"
 montage  -tile 5 -geometry +0+0  $framelist "$1".jpeg
 
-#feh testout.jpg
 echo "cleaning temp files.."
 rm _tmp*
 
