e23dea73845b9cad2c4f085f57c6beeead3e68d8
kirgahn@hotmail.com <kirgahn@hotmail.com>
2016-01-19 00:48:49 +0000

    added video2trailer-batch.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ cp video2gallery.py /usr/local/bin/video2gallery
 cp video2trailer.py /usr/local/bin/video2trailer
 cp video2webm.py /usr/local/bin/video2webm
 cp video2filmstrip.sh /usr/local/bin/video2filmstrip
+cp video2trailer-batch.sh /usr/local/bin/video2trailer-batch
 chown root. /usr/local/bin/video2*
 chmod 755 /usr/local/bin/video2*
