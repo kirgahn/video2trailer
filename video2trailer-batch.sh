1ac017d02ff610008607d10154c5473b83eac463
kirgahn@hotmail.com <kirgahn@hotmail.com>
2016-04-27 22:59:17 +0200

    tweaked video2trailer-batch defaults for 30 secs trailers

--- video2trailer-batch.sh
+++ video2trailer-batch.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 #echo test "$@"
-sliceduration=3
-clipduration=60
-width=800
+#sliceduration=3
+#clipduration=60
+#width=800
+
+sliceduration=2
+clipduration=29
+width=720
 
 for source in "$@";
 do
